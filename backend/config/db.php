a450151b68be8d8a7af8aac37be0ac22af21e697
Muqaddas Mehboob <muqaddasmehboob7@gmail.com>
2026-03-04 04:18:50 +0500

    Setup Mongodb in Backend

---
+++ backend/config/db.php
@@ -0,0 +1,18 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$uri = $_ENV['MONGODB_URI'];
+$dbName = $_ENV['DB_NAME'];
+
+try {
+    $client = new MongoDB\Client($uri);
+    $database = $client->$dbName;
+
+    echo "Connected successfully!";
+} catch (Exception $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
